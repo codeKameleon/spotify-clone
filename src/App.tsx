28cb2d1b9ee2aa58b9f28d115580604d7261237a
axel <tavormina.axel@gmail.com>
2022-03-02 11:00:06 +0100

    Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+  const getAuthURL = (): string => {
+    const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI } = process.env
+
+    const client_ID = REACT_APP_CLIENT_ID;
+    const redirect_URI =  REACT_APP_REDIRECT_URI;
+    const scopes = ["playlist-read-private", "playlist-modify-private", "user-read-recently-played"];
+
+    const auth_URL = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_ID}&scope=${scopes}&redirect_uri=${redirect_URI}`
+
+    return auth_URL
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <a href={getAuthURL()}>Authentication</a>
       </header>
     </div>
   );
